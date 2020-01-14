f3222a0dec91939eca44bdb3745ad3dcdb51c11f
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-01-13 21:50:25 -0500

    Show popup to desribe button actions dynamically

--- client/src/components/DeleteButton.js
+++ client/src/components/DeleteButton.js
@@ -3,6 +3,7 @@ import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
 import { Button, Confirm, Icon } from "semantic-ui-react";
 import { FETCH_POSTS_QUERY } from "../util/graphql";
+import MyPopup from "../util/MyPopup";
 
 function DeleteButton({ postId, commentId, callback }) {
     const [confirmOpen, setConfirmOpen] = useState(false);
@@ -34,9 +35,11 @@ function DeleteButton({ postId, commentId, callback }) {
 
     return (
         <>
-            <Button as="div" color="red" onClick={() => setConfirmOpen(true)} floated="right">
-                <Icon name="trash" style={{ margin: 0 }} />
-            </Button>
+            <MyPopup content={commentId ? "Delete comment" : "Delete post"}>
+                <Button as="div" color="red" onClick={() => setConfirmOpen(true)} floated="right">
+                    <Icon name="trash" style={{ margin: 0 }} />
+                </Button>
+            </MyPopup>
             <Confirm open={confirmOpen} onCancel={() => setConfirmOpen(false)} onConfirm={deletePostOrComment} />
         </>
     );
